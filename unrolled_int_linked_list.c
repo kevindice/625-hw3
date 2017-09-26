7e0747295975409738ca760c849b14ee67c7cd1a
Kevin Dice <kevin.dice1@gmail.com>
2017-09-26 02:20:50 -0500

    Start unrolled linked list implimentation

--- unrolled_int_linked_list.c
+++ unrolled_int_linked_list.c
@@ -2,6 +2,20 @@
 #include<stdlib.h>
 #define MAX_ELEMENTS 16
 
+/*
+ * Unrolled integer linked list
+ *
+ * This linked list implimentation is read and write
+ * only. No delete operations are implimented to make
+ * my life eaiser.
+ *
+ * This is an attempt at a cache and locality friendly,
+ * reasonably space-efficient way to store a sparce
+ * matrix of keyword search hits.
+ *
+ */
+
+
 // Unrolled Linked List Node
 struct Node
 {
@@ -24,4 +38,28 @@ void printUnrolledList(struct Node *n)
     }
 }
 
+// Find number of elements stored
+int getLength(struct Node *n)
+{
+    int len = 0;
+    while(n != NULL)
+    {
+        len += n->numElements;
+    }
+    return len;
+}
+
 // Covert linked list to array
+void toArray(struct Node *n, int **array, int *length)
+{
+    length = getLength(n);
+    array = malloc(length * sizeof(int));
+
+    int pos = 0;
+    while (n != NULL)
+    {
+	memcpy(array + pos, n->array, n->numElements * sizeof(int));
+	pos += n->numElements;
+	n = n->next;
+    }
+}
