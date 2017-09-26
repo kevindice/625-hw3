32b901e7a6e44199ddbc0d7525a01a9921c8b8b5
Kevin Dice <kevin.dice1@gmail.com>
2017-09-26 08:18:30 -0500

    Set up openmp version with latest serial code

--- omp.c
+++ omp.c
@@ -3,27 +3,44 @@
 #include <string.h>
 #include <time.h>
 #include <omp.h>
+#include "unrolled_int_linked_list.c"
 
 double myclock();
 
-int main()
+int compare(const void* a, const void* b) {
+    const char **ia = (const char **)a;
+    const char **ib = (const char **)b;
+    return strcmp(*ia, *ib);
+}
+
+int main(int argc, char * argv[])
 {
  /*  int nwords, maxwords = 50000;
    int nwords, maxwords = 50000; */
    int nwords, maxwords = 50000;
    int nlines, maxlines = 1000000;
-   int i, k, n, err, *count, nthreads = 4;
+   int i, k, n, err, *count;
    double nchars = 0;
    double tstart, ttotal;
    FILE *fd;
    char **word, **line;
+   struct Node** hithead;
+   struct Node** hitend;
+
+   if(argc != 3){
+      printf("Usage: %s <job id> <input size>", argv[0]);
+      return -1;
+   }
 
 // Malloc space for the word list and lines
 
    word = (char **) malloc( maxwords * sizeof( char * ) );
    count = (int *) malloc( maxwords * sizeof( int ) );
+   hithead = (struct Node**) malloc( maxwords * sizeof(struct Node *) );
+   hitend = (struct Node**) malloc( maxwords * sizeof(struct Node *) );
    for( i = 0; i < maxwords; i++ ) {
       word[i] = malloc( 10 );
+      hithead[i] = hitend[i] = (struct Node *) calloc(1, sizeof(struct Node));
       count[i] = 0;
    }
 
@@ -35,7 +52,7 @@ int main()
 
 // Read in the dictionary words
 
-   fd = fopen( "./keywords.txt", "r" );
+   fd = fopen( "/homes/kmdice/625/hw3/keywords.txt", "r" );
    nwords = -1;
    do {
       err = fscanf( fd, "%[^\n]\n", word[++nwords] );
@@ -47,15 +64,23 @@ int main()
 
 // Read in the lines from the data file
 
-   fd = fopen( "./test100.txt", "r" );
+   char *input_file = (char*)malloc(50 * sizeof(char));
+   sprintf(input_file, "/homes/kmdice/625/hw3/test10-%s.txt", argv[2]);
+   fd = fopen( input_file, "r" );
    nlines = -1;
    do {
       err = fscanf( fd, "%[^\n]\n", line[++nlines] );
       if( line[nlines] != NULL ) nchars += (double) strlen( line[nlines] );
    } while( err != EOF && nlines < maxlines);
    fclose( fd );
+   free(input_file);
 
    printf( "Read in %d lines averaging %.0lf chars/line\n", nlines, nchars / nlines);
+
+
+// sort
+
+   qsort(word, nwords, sizeof(char *), compare);
 
 
 // Loop over the word list
@@ -63,30 +88,57 @@ int main()
    tstart = myclock();  // Set the zero time
    tstart = myclock();  // Start the clock
 
-   omp_set_num_threads( nthreads );
-
 #pragma omp parallel for schedule( dynamic ) private(i,k)
-   for( i = 0; i < nwords; i++ ) {
-
-      for( k = 0; k < nlines; k++ ) {
-         if( strstr( line[k], word[i] ) != NULL ) count[i]++;
+   for( k = 0; k < nlines; k++ ) {
+      for( i = 0; i < nwords; i++ ) {
+         if( strstr( line[k], word[i] ) != NULL ) {
+	    count[i]++;
+	    hitend[i] = add(hitend[i], k);
+	 } 
       }
 
    }
 
    ttotal = myclock() - tstart;
-   printf( "The run on %d cores took %lf seconds for %d words\n",
-           nthreads, ttotal, nwords);
+   printf( "The flipped serial run took %lf seconds for %d words over %d lines\n",
+           ttotal, nwords, nlines);
 
 // Dump out the word counts
 
-   fd = fopen( "wiki.out", "w" );
+   char *output_file = (char*)malloc(50 * sizeof(char));
+   sprintf(output_file, "/homes/kmdice/625/hw3/output/wiki-%s.out", argv[1]);
+   fd = fopen( output_file, "w" );
    for( i = 0; i < nwords; i++ ) {
-      fprintf( fd, "%d %s %d\n", i, word[i], count[i] );
+      if(count[i] != 0){
+         fprintf( fd, "%d %s (%d): ", i, word[i], count[i] );
+         int *line_numbers;
+         int len;
+         toArray(hithead[i], &line_numbers, &len);
+         for (k = 0; k < len - 1; k++) {
+            fprintf( fd, "%d, ", line_numbers[k]);
+         }
+         fprintf( fd, "%d\n", line_numbers[len - 1]);
+         free(line_numbers);
+      }
    }
-   fprintf( fd, "The run on %d cores took %lf seconds for %d words\n",
-           nthreads, ttotal, nwords);
+   fprintf( fd, "The flipped serial run took %lf seconds for %d words over %d lines\n",
+           ttotal, nwords, nlines);
    fclose( fd );
+
+// Clean up after ourselves
+
+   for(i = 0; i < maxwords; i++ ) {
+      free(word[i]);
+      destroy(hithead[i]);
+   }
+   free(word);
+   free(hithead);
+   free(hitend);
+
+   for(i = 0; i < maxlines; i++ ) {
+      free(line[i]);
+   }
+   free(line);
 
 }
 
