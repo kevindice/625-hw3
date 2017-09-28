959b03e7b9d277e76bab1c77fa2cd5da513d61f4
Kevin Dice <kevin.dice1@gmail.com>
2017-09-28 16:23:16 -0500

    Clean up mpi run folder

--- run-mpi/sub-16-fill.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -pe mpi-fill 16
-#$ -l mem=3G
-#$ -l h_rt=0:30:00
-#$ -cwd
-#$ -j y
-#$ -m abe
-#$ -M kevin.dice1@gmail.com
-mpirun -np 16 /homes/kmdice/625/hw3/mpi $JOB_ID 6
--- run-mpi/sub-64-fill-small.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -pe mpi-fill 64
-#$ -l mem=3G
-#$ -l h_rt=0:07:00
-#$ -cwd
-#$ -j y
-#$ -m abe
-#$ -M kevin.dice1@gmail.com
-mpirun -np 64 /homes/kmdice/625/hw3/mpi $JOB_ID 4
