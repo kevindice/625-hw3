d165538106d24a3863f7693977ba71620bed7339
Kevin Dice <kevin.dice1@gmail.com>
2017-09-28 19:17:28 -0500

    remove extension

--- run-all/mpi-run.sh
+++ run-all/mpi-run.sh
@@ -5,13 +5,10 @@
 #$ -j y
 #$ -m abe
 #$ -M kevin.dice1@gmail.com
-mpirun -np $NSLOTS /homes/kmdice/625/hw3/mpi "{$JOB_ID}_6" 6 "$PE" $NSLOTS $NHOSTS
-mpirun -np $NSLOTS /homes/kmdice/625/hw3/mpi "{$JOB_ID}_5" 5 "$PE" $NSLOTS $NHOSTS
-mpirun -np $NSLOTS /homes/kmdice/625/hw3/mpi "{$JOB_ID}_4" 4 "$PE" $NSLOTS $NHOSTS
-mpirun -np $NSLOTS /homes/kmdice/625/hw3/mpi "{$JOB_ID}_3" 3 "$PE" $NSLOTS $NHOSTS
 
 for x in 3 4 5 6
 do
-    cat /homes/kmdice/625/hw3/output/wiki-{$JOB_ID}_$x-part-*.out > /homes/kmdice/625/hw3/output/{$JOB_NAME}_{$JOB_ID}_$x.out
-    rm /homes/kmdice/625/hw3/output/wiki-{$JOB_ID}_$x-part-*.out
+    mpirun -np $NSLOTS /homes/kmdice/625/hw3/mpi "${JOB_ID}_${x}" $x "$PE" $NSLOTS $NHOSTS
+    cat /homes/kmdice/625/hw3/output/wiki-${JOB_ID}_${x}-part-* > /homes/kmdice/625/hw3/output/${JOB_NAME}_${JOB_ID}_$x.out
+    rm /homes/kmdice/625/hw3/output/wiki-${JOB_ID}_${x}-part-*
 done
