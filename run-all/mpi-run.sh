1e31b6713feb65e341f964edd2e31b9404721093
Kevin Dice <kevin.dice1@gmail.com>
2017-09-30 07:45:32 -0500

    Add -x LD_PRELOAD flag

--- run-all/mpi-run.sh
+++ run-all/mpi-run.sh
@@ -7,7 +7,7 @@
 
 for x in 6 5 4 3
 do
-    mpirun -np $NSLOTS /homes/kmdice/625/hw3/mpi "${JOB_ID}_${x}" $x "$PE" $NSLOTS $NHOSTS
+    mpirun -x LD_PRELOAD=libmpi.so -np $NSLOTS /homes/kmdice/625/hw3/mpi "${JOB_ID}_${x}" $x "$PE" $NSLOTS $NHOSTS
     cat /homes/kmdice/625/hw3/output/wiki-${JOB_ID}_${x}-part-* > /homes/kmdice/625/hw3/output/${JOB_NAME}_${JOB_ID}_$x.out
     rm /homes/kmdice/625/hw3/output/wiki-${JOB_ID}_${x}-part-*
 done
