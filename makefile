01c3e07144a5d4e0c5080de0b6c02908d4db5e89
Kevin Dice <kevin.dice1@gmail.com>
2017-10-02 15:22:35 -0500

    Add optimization to mpi run and add dwarf script

--- makefile
+++ makefile
@@ -16,7 +16,7 @@ serial: serial.c unrolled_int_linked_list.c
 	cc serial.c -o serial $(FLAGS)
 
 mpi: mpi.c unrolled_int_linked_list.c
-	mpicc mpi.c -o mpi $(FLAGS)
+	mpicc mpi.c -o mpi $(FLAGS) -O2
 
 test_linked_list: test_linked_list.c unrolled_int_linked_list.c
 	cc test_linked_list.c -O0 -o test_linked_list
---
+++ run-all/dwarf-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -l mem=3G
+#$ -q \*@@dwarves
+#$ -cwd
+#$ -j y
+#$ -m abe
+#$ -M kevin.dice1@gmail.com
+
+for x in 6
+do
+    mpirun -x LD_PRELOAD=libmpi.so -np $NSLOTS /homes/kmdice/625/hw3/mpi "${JOB_ID}_${x}" $x "$PE" $NSLOTS $NHOSTS
+    cat /homes/kmdice/625/hw3/output/wiki-${JOB_ID}_${x}-part-* > /homes/kmdice/625/hw3/output/${JOB_NAME}_${JOB_ID}_$x.out
+    rm /homes/kmdice/625/hw3/output/wiki-${JOB_ID}_${x}-part-*
+done
